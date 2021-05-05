389168092e5716235cedff4686ccb54e2a99d6a2
lorelles <74384017+lorelles@users.noreply.github.com>
2021-05-04 19:30:49 -0700

    dropdown

--- app.js
+++ app.js
@@ -5,25 +5,64 @@ d3.json("samples.json").then(data => {
     data.names.forEach(id => {
         select.append("option")
             .text(id)
-            .property("value")
+            .property("value", id)
     });
 });
 
+// let id = "940"
+// function init() {
+    // createCharts("940");
+// }
 
-function optionChanged(userValue) {
-    d3.json('samples.json').then(data => {
-        console.log(userValue)
-        createCharts(data, userValue)
-    });
-}
+//     d3.json("samples.json").then(data => {
+//     const userValue = d3.select("#selDataset");
+
+// });
+
+//Function to get the new county when the user selects a new county
+// function userValue() {
+//     // d3.json('samples.json').then(data => {
+//     let dropdownMenu = d3.select("#selDataset");
+//     let dropID = dropdownMenu.property("value");
+
+//     createCharts(dropID);
+//     // });
+// }
+
+// function init() {
+//     d3.json('samples.json').then(data => {
+        
+//         // const userValue = d3.select("#selDataset");
+//         createCharts(data); 
+//         console.log(data);
+//         Plotly.newPlot("bar", data);
+//     });
+// };
+
+// function optionChanged(userValue) {
+//     d3.json('samples.json').then(data => {
+//         // let userValue = d3.event.target.value
+
+//         // const userValue = d3.select("#selDataset");
+//         createCharts(data, userValue); 
+//         console.log(userValue);
+//         // buildPlot(data, userValue)
+//     });
+// }
+
+
 
 // buildPlot();
-function createCharts(data) {
+function createCharts(id) {
+    // d3.json('samples.json').then(data => {
+    
 
     // Call in data, assign to variables
-    // d3.json('samples.json').then(function(data) {
-
-    let id = data.names;
+    d3.json('samples.json').then(function(data) {
+        
+    console.log(data);
+    let name = data[0];
+    // let id = data.metadata.id;
     let ethnicity = data.metadata[0].ethnicity;
     let gender = data.metadata[0].gender;
     let age = data.metadata[0].age;
@@ -37,31 +76,50 @@ function createCharts(data) {
     let text_otu = data.samples[0].otu_labels.slice(0, 10).sort((a, b) => a - b);
     let color_otu = data.samples[0].otu_ids.slice(0, 10).sort((a, b) => a - b);
     
+    console.log(name);
+    // console.log(value_otu);
+    // console.log(label_otu);
+    // console.log(text_otu);
+    // console.log(color_otu);
+
+    // let select = d3.select("#selDataset")
+    // data.names.forEach(id => {
+    //     select.append("option")
+    //         .text(id)
+    //         .property("value", id)           
+    // });
+
     // create table
     let table = d3.select("#sample-metadata");
     let tbody = table.select("tbody");  
     tbody.html("");
     
     let trow;
-    for (let i = 0; i < 2; i++) {
-        // data.metadata[0].id.forEach(id => {
+    // trow = tbody.append("tr");
+    // data.names.forEach(id => {
+
+    // for (let i = 0; i < 2; i++) {
+        // data.names.forEach(id => {
         trow = tbody.append("tr");
         // trow.append("td").text(data.metadata[i]);
-        trow.append("tr").text("id: ").append("td").text(data.metadata[i].id);
+        trow.append("tr").text("id: ").append("td").text(id);
+
+        // table.append("tr").text("id: ").append("td").text(data.metadata[i].id);
         // trow.append("tr").text(data.metadata[i].id);
-        trow.append("tr").text("ethnicity: ").append("td").text(data.metadata[i].ethnicity);
+        trow.append("tr").text("ethnicity: ").append("td").text(ethnicity);
         // trow.append("tr").text(data.metadata[i].ethnicity);
-        trow.append("tr").text("gender: ").append("td").text(data.metadata[i].gender); 
+        trow.append("tr").text("gender: ").append("td").text(gender); 
         // trow.append("tr").text(data.metadata[i].gender);
-        trow.append("tr").text("age: ").append("td").text(data.metadata[i].age);
+        trow.append("tr").text("age: ").append("td").text(age);
         // trow.append("tr").text(data.metadata[i].age);
-        trow.append("tr").text("location: ").append("td").text(data.metadata[i].location);
+        trow.append("tr").text("location: ").append("td").text(location);
         // trow.append("tr").text(data.metadata[i].location);
-        trow.append("tr").text("bbtype: ").append("td").text(data.metadata[i].bbtype);
+        trow.append("tr").text("bbtype: ").append("td").text(bbtype);
         // trow.append("tr").text(data.metadata[i].bbtype);
-        trow.append("tr").text("wfreq: ").append("td").text(data.metadata[i].wfreq);
+        trow.append("tr").text("wfreq: ").append("td").text(wfreq);
         // trow.append("tr").text(data.metadata[i].wfreq);
-    };
+    // };
+    
     // });
 
     // Bar Chart
@@ -108,8 +166,17 @@ function createCharts(data) {
     };
 
     Plotly.newPlot("bubble", plotData2, layout2);
-
+});
+    
 };
+
+function optionChanged() {
+        let select = d3.select("#selDataset");
+        let testID = select.property("value");
+        console.log(testID);
+        createCharts(testID);
+    }
+
 
 // add event listener to select element
 d3.selectAll("#selDataset").on("change", optionChanged);
@@ -118,9 +185,14 @@ d3.selectAll("#selDataset").on("change", optionChanged);
 //     let name = d3.event.target.value
 //     let nameData = data.metadata.name.find(d => name == id)
 //     init(nameData);
+//     console.log(nameData)
 // }
-// init(data.metadata[0]);
 
-d3.json("samples.json").then(data => {
-    createCharts(data, "940");
-});
+// init(data.metadata[0]);
+// init();
+
+// d3.json("samples.json").then(data => {
+//     createCharts(data, "940");
+// });
+
+
